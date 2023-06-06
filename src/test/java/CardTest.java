e5ac532d85d79d7930eeeac9f3a68f2617c9eec5
Greg Mitchell <g2mitchell3003@gmail.com>
2023-06-06 08:43:28 -0600

    Change assertion type

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,14 +1,13 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Test.*;
 
 class CardTest {
     @Test
     void setsAttrs() {
         Card card = new Card("Hearts", "Queen", 12);
 
-        assertTrue(card.rank == 12);
+        assertEquals(12, card.rank);
         assertTrue(card.suit == "Hearts");
         assertTrue(card.value == "Queen");
     }
